3448f9a617ef6d5fafbb0ef9ec4207ba27912a05
Yuya Kimura <107418519+kimukimuyuya@users.noreply.github.com>
2024-03-06 14:01:10 +0900

    第3章終了

--- assignment/Ch3_Kimura.R
+++ assignment/Ch3_Kimura.R
@@ -12,3 +12,21 @@ dev.off()
 #相関係数の計算
 result_cor <- cor.test(ip, cc)
 result_cor
+
+#回帰分析
+result_lm <- lm(ip ~ cc)
+summary(result_lm)
+
+#政府のパフォーマンスと南北地域
+t.test(ip ~ ns, var.equal = TRUE)
+#政府パフォーマンスと近代化の度合いの単回帰分析
+result_lm2 <- lm(ip ~ em)
+summary(result_lm2)
+
+#政府パフォーマンスと近代化の度合い+南北地域の重回帰分析
+result_lm6 <- lm(ip ~ em + ns)
+summary(result_lm6)
+
+#政府パフォーマンスと社会資本関係+南北地域の重回帰分析
+result_lm7 <- lm(ip ~ cc + ns)
+summary(result_lm7)
